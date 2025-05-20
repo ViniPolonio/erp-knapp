d3adb20616ab574d86f8bd9895792a0293e63d7a
ViniPolonio <poloniober@gmail.com>
2025-05-20 14:54:55 -0300

    update project

---
+++ src/components/Layout/Company/Company.jsx
@@ -0,0 +1,52 @@
+// src/layout/Users.jsx
+import { NavLink, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { FaUsers, FaChevronDown, FaChevronRight, FaUserPlus, FaUserEdit, FaUserCheck } from "react-icons/fa";
+
+const CompanyMenu = () => {
+  const location = useLocation();
+  const isUsersSection = location.pathname.startsWith("/users");
+
+  const [isOpen, setIsOpen] = useState(isUsersSection);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <li onClick={toggleMenu} className={`nav-item ${isUsersSection ? "active" : ""}`} style={{ cursor: "pointer" }}>
+        <span className="nav-icon"><FaUsers /></span>
+        Usuários
+        <span style={{ marginLeft: "auto" }}>
+          {isOpen ? <FaChevronDown size={12} /> : <FaChevronRight size={12} />}
+        </span>
+      </li>
+
+      {isOpen && (
+        <ul className="submenu submenu-shadow">          
+          <li>
+            <NavLink 
+              to="/users/edit"
+              className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
+            >
+              <span className="nav-icon user-option-special"><FaUserEdit /></span>
+              Editar
+            <span className="user-option-separator"></span></NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/users-approve"
+              className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
+            >
+              <span className="nav-icon user-option-special"><FaUserCheck /></span>
+              Aprovar
+            <span className="user-option-separator"></span></NavLink>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default CompanyMenu;
